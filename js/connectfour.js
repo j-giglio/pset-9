4269821125d3151c6fdba282e9882c9b0c8bf6c7
Amy M. Giglio <agiglio@PARISH.LOCAL>
2020-02-27 22:38:02 -0500

    got conn4 buttons to work

--- js/connectfour.js
+++ js/connectfour.js
@@ -16,6 +16,7 @@ let columns = [
   [null, null, null, null, null, null]
 ]
 
+let turn;
 let redWin = 0;
 let yellowWin = 0;
 let tieCount = 0;
@@ -25,37 +26,37 @@ let tieCount = 0;
 const message = document.querySelector("h2");
 const board = document.getElementById("connectfour-board")
 const messagePartOne = document.getElementById("messagePartOne");
-const redButton = document.getElementById("red-button");
+const redButton = document.getElementById("button-red");
 const messagePartTwo = document.getElementById("messagePartTwo");
-const yellowButton = document.getElementById("yellow-button");
+const yellowButton = document.getElementById("button-yellow");
 const messagePartThree = document.getElementById("messagePartThree");
 const canvas = document.getElementById('connectfour-board');
 const ctx = canvas.getContext("2d");
+const winCount = document.getElementById("winCount")
 
 ///////////////////// EVENT LISTENERS ///////////////////////////////
 
-window.onload = /*init;*/ render;
+window.onload = init;
 //
 // document.getElementById("board").onclick = takeTurn;
-// document.getElementById("reset-button").onclick = init;
-// document.getElementById("win-reset").onclick = reset;
-// redButton.onclick = setTurn;
-// yellowButton.onclick = setTurn;
+document.getElementById("reset-button").onclick = init;
+document.getElementById("win-reset").onclick = reset;
+redButton.onclick = setTurn;
+yellowButton.onclick = setTurn;
 
 ///////////////////// FUNCTIONS /////////////////////////////////////
 
-// function init() {
-//   message.appendChild(messagePartOne);
-//   message.appendChild(redButton);
-//   message.appendChild(messagePartTwo);
-//   message.appendChild(yellowButton);
-//   message.appendChild(messagePartThree); /*this whole setup is so janky but it works*/
-//   win = null;
-//
-//   if (turn) {
-//     render();
-//   }
-// }
+function init() {
+  message.appendChild(messagePartOne);
+  message.appendChild(redButton);
+  message.appendChild(messagePartTwo);
+  message.appendChild(yellowButton);
+  message.appendChild(messagePartThree); /*this whole setup is so janky but it works*/
+  win = null;
+
+  render();
+
+}
 
 function render() {
   let xCoord = 80;
@@ -84,15 +85,15 @@ function render() {
 //   //   tieCount++
 //   // }
 // }
-//
-// function reset() {
-//   redWin = 0;
-//   yellowWin = 0;
-//   tieCount = 0;
-//   winCount.textContent = "Red: 0 | Yellow: 0 | Tie: 0";
-// }
-//
-// function setTurn(f) {
-//   turn = f.target.id.startsWith
-//   message.textContent = `Turn: ${turn}`;
-// }
+
+function reset() {
+  redWin = 0;
+  yellowWin = 0;
+  tieCount = 0;
+  winCount.textContent = "Red: 0 | Yellow: 0 | Tie: 0";
+}
+
+function setTurn(f) {
+  turn = f.target.id.startsWith
+  message.textContent = `Turn: ${turn}`;
+}
