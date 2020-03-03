4f31d8c768a8752d41be9478a023965a50c18a78
Amy M. Giglio <agiglio@PARISH.LOCAL>
2020-03-02 22:44:51 -0500

    got ball to bounce

--- js/brickbreaker.js
+++ js/brickbreaker.js
@@ -1,7 +1,7 @@
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
 
 const message = document.querySelector("h2");
-const canvas = document.querySelector("canvas"));
+const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const winCount = document.getElementById("winCount")
 
@@ -30,9 +30,44 @@ function init() {
   ball = {
     radius: 10,
     x: canvas.width / 2,
-    y: canvas.height - 30
+    y: canvas.height - 30,
+    deltaX: 2,
+    deltaY: -2,
   }
+
   gameStarted = true;
+  setInterval(drawBall, 10);
+}
+
+function drawBall() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI*2);
+  ctx.fillStyle = "#952000";
+  ctx.fill();
+  ctx.closePath();
+  ball.x += ball.deltaX;
+  ball.y += ball.deltaY;
+
+  if (ball.y === canvas.height - 10) {
+    gameOver();
+  }
+
+  if (ball.y === 10) {
+    updateDeltaY();
+  }
+
+  if (ball.x === canvas.width - 10 || ball.x === 10) {
+    updateDeltaX();
+  }
+}
+
+function updateDeltaX() {
+  ball.deltaX += -2 * ball.deltaX;
+}
+
+function updateDeltaY() {
+  ball.deltaY += -2 * ball.deltaY;
 }
 
 function movePaddle() {
@@ -50,6 +85,7 @@ function movePaddle() {
 
 function gameOver() {
   gameStarted = false;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 function victory() {
